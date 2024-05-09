70bfef4aaf8de707d54b5027cb8d7b99a624e97c
johnbwalkey <john@walkey.ca>
2024-05-09 11:01:44 -0600

    new

---
+++ gui X_O game.py
@@ -0,0 +1,65 @@
+# gui X_O game from chatgtp
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Tic Tac Toe")
+
+        self.current_player = 'X'
+        self.board = [['' for _ in range(3)] for _ in range(3)]
+
+        self.buttons = [[None for _ in range(3)] for _ in range(3)]
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j] = tk.Button(master, text='', font=('Helvetica', 30), width=5, height=2,
+                                                command=lambda i=i, j=j: self.on_click(i, j))
+                self.buttons[i][j].grid(row=i, column=j)
+
+    def on_click(self, row, col):
+        if self.board[row][col] == '':
+            self.board[row][col] = self.current_player
+            self.buttons[row][col].config(text=self.current_player)
+            if self.check_winner():
+                messagebox.showinfo("Tic Tac Toe", f"{self.current_player} wins!")
+                self.reset_game()
+            elif self.check_draw():
+                messagebox.showinfo("Tic Tac Toe", "It's a draw!")
+                self.reset_game()
+            else:
+                self.current_player = 'O' if self.current_player == 'X' else 'X'
+
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != '':
+                return True
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != '':
+                return True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != '':
+            return True
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != '':
+            return True
+        return False
+
+    def check_draw(self):
+        for row in self.board:
+            for cell in row:
+                if cell == '':
+                    return False
+        return True
+
+    def reset_game(self):
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j].config(text='')
+                self.board[i][j] = ''
+        self.current_player = 'X'
+
+def main():
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
